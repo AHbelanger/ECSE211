a051b75ddfee00df9169d17a34cf183008f8d41a
Sabrina Chan <sabrina.chan3@mail.mcgill.ca>
2019-09-20 12:58:58 -0400

    odometry correction friday

--- Lab2_Odometry/src/ca/mcgill/ecse211/lab2/OdometryCorrection.java
+++ Lab2_Odometry/src/ca/mcgill/ecse211/lab2/OdometryCorrection.java
@@ -11,109 +11,99 @@ import lejos.hardware.sensor.SensorModes;
 import lejos.robotics.SampleProvider;
 
 public class OdometryCorrection implements Runnable {
- 
-  private static final long CORRECTION_PERIOD = 10;
-  private Odometer odometer;
- 
-  private static final Port colorSampler = LocalEV3.get().getPort("S1");
-  private SensorModes colosSamplerSensor = new EV3ColorSensor(colorSampler);
-  private SampleProvider colorSensorValue = colosSamplerSensor.getMode("Red");
 
-  private float[] colorSensorData = new float[colosSamplerSensor.sampleSize()];
-  
+  private static final long CORRECTION_PERIOD = 10; // time between corrections
+  private Odometer odometer; 
+
+  // creating the light sensor
+  private static final Port colorSampler = LocalEV3.get().getPort("S1"); // get the port for the light sensor
+  private SensorModes colorSamplerSensor = new EV3ColorSensor(colorSampler); // get instance of sensor
+  private SampleProvider colorSensorValue = colorSamplerSensor.getMode("Red"); // get sample provider
+  private float[] colorSensorData = new float[colorSamplerSensor.sampleSize()]; // create data buffer
+
   private float oldValue = 0;
-  private int counterX;
-  private int counterY;
-  private double theta;
-  
-  // constructor
-  public OdometryCorrection(Odometer odometer) {
-      this.odometer = odometer;
+  private int counterX; // counts how many lines in x
+  private int counterY; // counts how many lines in y
+  private double theta; // angle
+
+  private static int counter = 0; //counts how many times the light sensor has seen a line
+  private static int SQUARE_SIDES = 4;
+  private static ArrayList<Double> increments = new ArrayList<Double>(); //will hold our increment values
+
+
+  /**
+   *  constructor for the odometer
+   *  ensures that the thread runs safely
+   */
+  public OdometryCorrection() {
+    this.odometer = Odometer.getOdometer();
   }
 
+  /**
+   * where the run method will execute
+   * needs this to run since it is in a thread
+   * when a line is detected, the robot will adjust the position
+   */
   public void run() {
     long correctionStart, correctionEnd;  
-          
-      // TODO Trigger correction (When do I have information to correct?)
-    while (true) {
-      correctionStart = System.currentTimeMillis();
-      
-      //fetching the values from the color sensor
-      colorSensorValue.fetchSample(colorSensorData, 0);
-      
-      //getting the value returned from the sensor, and multiply it by 1000 to scale
-      float value = colorSensorData[0]*1000;
-      
-      //computing the derivative at each point
-      float diff = value - oldValue;
-      
-      //storing the current value, to be able to get the derivative on the next iteration
-      oldValue = value;
-      
-      //if the derivative value at a given point is less than -50, this means that a black line is detected
-      if(diff < -50) {
-          
-          //robot beeps
-          Sound.beep();
-          
-          //get the status of the robot by the counter used in squareDriver, this counter keeps track of the orientation of the robot
-          //int status = SquareDriver.getSquareCount();
-          
-       //   theta = odometer.getTheta() * 180 / Math.PI;
-          /*
-           * The Y and X counter keeps track of how many horizontal and vertical black lines the robot detected respectively.
-           * This counter is used to computer the Y and X values respect to the origin (first intersection).
-           * theta tells us if the robot is moving forward horizontally or vertically.
-           */
-          
-      /*  if(status == 0){
-              odometer.setY(counterY * TILE_LENGTH);
-              counterY ++;
-          }else if(status == 1 ){
-              odometer.setX(counterX * TILE_LENGTH);
-              counterX++;
-          }else if(status == 2){
-              counterY--;
-              odometer.setY(counterY * TILE_LENGTH);                  
-          }else if(status == 3){
-              counterX--;
-              odometer.setX(counterX * TILE_LENGTH);
-          }   */
-          
-          if( (theta <= 360 && theta >= 315) || (theta >=0 && theta <= 45)){
-              odometer.setY(counterY * TILE_SIZE);
-              counterY++;
-          }else if(theta > 45 && theta <= 135){
-              odometer.setX(counterX * TILE_SIZE);
-              counterX++;
-          }else if(theta > 135 && theta <= 225){
-              counterY--;
-              odometer.setY(counterY * TILE_SIZE);
-          }else if(theta > 225 && theta < 315){
-              counterX--;
-              odometer.setX(counterX * TILE_SIZE);
-          }
-          
-          try {
-              Thread.sleep(500);
-          } catch (InterruptedException e) {
-              // TODO Auto-generated catch block
-              e.printStackTrace();
-          }
-      }
-          
-    
-      // TODO Update odometer with new calculated (and more accurate) values, eg:
-      //odometer.setXYT(0.3, 19.23, 5.0);
-      
-      
 
-      // this ensures the odometry correction occurs only once every period
-      correctionEnd = System.currentTimeMillis();
-      if (correctionEnd - correctionStart < CORRECTION_PERIOD) {
-        Main.sleepFor(CORRECTION_PERIOD - (correctionEnd - correctionStart));
+    //I initialize the array with the appropriate values
+    for (int i=0; i< (SQUARE_SIDES-1)*4; i++) {
+
+      if (i<(SQUARE_SIDES-1)) { //first side
+        increments.add(i*TILE_SIZE);
+      } else if (i<(SQUARE_SIDES-1)*2) { //second side
+        increments.add((i % 3)*TILE_SIZE);
+      } else if (i<(SQUARE_SIDES-1)*3) { //third side
+        increments.add((SQUARE_SIDES - 2 - i%3)*TILE_SIZE);
+      } else if (i<(SQUARE_SIDES-1)*4) { //last side
+        increments.add((SQUARE_SIDES - 2 - i%3)*TILE_SIZE);
       }
     }
+
+    // TODO Trigger correction (When do I have information to correct?)
+    while (true) {
+      correctionStart = System.currentTimeMillis();
+
+
+      //fetching the values from the color sensor
+      colorSensorValue.fetchSample(colorSensorData, 0);
+
+      if (colorSensorData[0] <= 0.35) { // if we are over a black line
+
+        Sound.beep(); // alerts us that we are over a line
+
+        if (counter < (SQUARE_SIDES - 1) || (counter >= (SQUARE_SIDES - 1)*2 && counter < (SQUARE_SIDES-1)*3)) { 
+          //We adjust Y on the first and third side of the square
+          odometer.setY(increments.get(counter));
+        } else if ((counter >= (SQUARE_SIDES - 1) && counter < (SQUARE_SIDES -1)*2) || (counter >= (SQUARE_SIDES -1)*3 && counter< (SQUARE_SIDES - 1)*4)){
+          //We adjust X
+          odometer.setX(increments.get(counter));
+        }
+
+        counter++;
+
+      }
+
+
+      try {
+        Thread.sleep(500);
+      } catch (InterruptedException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+
+
+    // TODO Update odometer with new calculated (and more accurate) values, eg:
+    //odometer.setXYT(0.3, 19.23, 5.0);
+
+    // this ensures the odometry correction occurs only once every period
+    correctionEnd = System.currentTimeMillis();
+    if (correctionEnd - correctionStart < CORRECTION_PERIOD) {
+      Main.sleepFor(CORRECTION_PERIOD - (correctionEnd - correctionStart));
+    }
   }
-  
 }
+
+
